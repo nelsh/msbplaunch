5ce02f0c652bacc862c22e0ba7425451b5cb8662
nelsh <nelsh@localhost>
2012-07-09 14:14:48 +0900

    Create empty console project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace msbplaunch
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
